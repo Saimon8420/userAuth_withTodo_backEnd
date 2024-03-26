f73d6db3704a03bb98685cb1cc4f51faf1cbf3e0
Saimon8420 <latifulkabir567@gmail.com>
2024-03-27 00:36:41 +0600

    cors error fixed

--- index.js
+++ index.js
@@ -10,28 +10,41 @@ const todoRoute = require("./router/todoRouter");
 const dbConnection = require("./helper/dbConnect");
 const cookieParser = require("cookie-parser");
 
-const allowedOrigins = ["https://benevolent-bonbon-a47d48.netlify.app/"];
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", allowedOrigins);
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+const allowedOrigins = ["https://benevolent-bonbon-a47d48.netlify.app/", "http://localhost:5173"];
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", allowedOrigins);
+//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials: true")
+//     next();
+// });
 
 // Whitelisted origins for CORS (replace with your allowed origins)
 
 
-app.use(cors({
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) !== -1) {
+//             return callback(null, true);
+//         } else {
+//             return callback(new Error('Origin not allowed by CORS'));
+//         }
+//     },
+//     credentials: true
+// }));
+const corsOptions = {
     origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            return callback(null, true);
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
         } else {
-            return callback(new Error('Origin not allowed by CORS'));
+            callback(new Error('Origin not allowed by CORS'));
         }
     },
     credentials: true
-}));
+};
+
+app.use(cors(corsOptions));
 
 app.use(cookieParser());
 
--- middlewares/setCookies.js
+++ middlewares/setCookies.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const setCookies = async (token, res) => {
     // setting http-only cookies
     const expiryDate = 60 * 60 * 1000;
     // Set the cookie with a more secure SameSite attribute (consider 'none' for HTTPS)
     await res.cookie('userAuth', token, {
         httpOnly: true,
-        secure: true, // Change to true for HTTPS
+        secure: process.env.NODE_ENV === 'production', // Change to true for HTTPS
         maxAge: expiryDate,
         sameSite: "lax" // 'none' for HTTPS with proper security measures
     });
